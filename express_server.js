16711cea8a8c502dcc07602ff3c4ce6e9c197f3a
mattwong59 <mattwong59@gmail.com>
2018-03-21 19:49:38 +0000

    Adds post request to server and sets cookie as username input

--- express_server.js
+++ express_server.js
@@ -60,6 +60,11 @@ app.post ("/urls/:id", (req, res) => {
   res.redirect ("/urls");
 });
 
+app.post ("/login", (req, res) => {
+  res.cookie(req.body);
+  res.redirect("/urls");
+});
+
 app.listen (PORT, () => {
   console.log (`Example app listening on port ${PORT}!`);
 });
